4f95979a6448d9f9017c49d0683c7c257e4b64d3
jram828 <128335805+jram828@users.noreply.github.com>
2024-08-05 18:57:10 -0500

    remove auth handler

--- Server/src/routes/userRoutes.js
+++ Server/src/routes/userRoutes.js
@@ -7,13 +7,13 @@ import {
   getAllUser,
 } from "../controllers/userController.js";
 import {verifyEmailHandler } from "../handlers/emailHandler.js";
-import { authHandler } from "../handlers/authhandler.js";
+// import { authHandler } from "../handlers/authhandler.js";
 
 export const router = express.Router();
 
 router.get("/", getAllUser);
 
 router.post("/register", registerUserHandler);
-router.post("/validate-user", authHandler);
+// router.post("/validate-user", authHandler);
 router.post("/recovery-password", recoveryPasswordHandler);
 router.post("/verify-email", verifyEmailHandler);
