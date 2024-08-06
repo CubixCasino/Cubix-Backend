81e7e2bcf027c5870f8664497eca693029d26dd6
jram828 <128335805+jram828@users.noreply.github.com>
2024-08-05 19:20:54 -0500

    fix conexion DB

--- Server/DB.js
+++ Server/DB.js
@@ -1,17 +1,19 @@
 import { Sequelize } from "sequelize";
 import { envs } from "./src/config/enviroments/enviroments.js";
 
+//! Configuración de Sequelize para despliegue en Render
 
 const sequelize = new Sequelize(envs.DB_URI, {
   logging: false,
   native: false,
-  dialectOptions: envs.DB_USE_SSL === 'true' ? {
+  dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false, // Esto puede ser necesario si estás usando certificados autofirmados
     },
-  } : {},
+  } ,
 });
+
 
 const models = {
   ...sequelize.models,
