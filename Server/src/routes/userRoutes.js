8494eaab3200c353370c93b204fc5514ce159a41
jram828 <128335805+jram828@users.noreply.github.com>
2024-08-05 19:35:33 -0500

    try to restore auth handler

--- Server/src/routes/userRoutes.js
+++ Server/src/routes/userRoutes.js
@@ -7,6 +7,7 @@ import {
   getAllUser,
 } from "../controllers/userController.js";
 import {verifyEmailHandler } from "../handlers/emailHandler.js";
+import { authHandler } from "../handlers/authHandler.js";
 // import { authHandler } from "../handlers/authhandler.js";
 
 export const router = express.Router();
@@ -14,6 +15,6 @@ export const router = express.Router();
 router.get("/", getAllUser);
 
 router.post("/register", registerUserHandler);
-// router.post("/validate-user", authHandler);
+router.post("/validate-user", authHandler);
 router.post("/recovery-password", recoveryPasswordHandler);
 router.post("/verify-email", verifyEmailHandler);
